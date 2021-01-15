d495307563c73e9865400deb70476af332155d75
Nizar Masmoudi <nizar.masmoudi@esprit.tn>
2021-01-15 17:46:58 +0100

    Update env.py

--- utils/env.py
+++ utils/env.py
@@ -1,7 +1,7 @@
 
-# ! Note : constants ending with "_" are relative paths. Otherwise, they are either absolute paths or just not paths
+# Note : constants ending with "_" are relative paths. Otherwise, they are either absolute paths or just not paths
 DATASET_ = './VisDrone2019/'
 TRAIN_PATH_ = DATASET_ + 'train/'
 TEST_PATH_ = DATASET_ + 'test/'
 VALIDATION_PATH_ = DATASET_ + 'validation/'
-MAIN_FOLDER = 'C:/Users/nizar/OneDrive/Documents/GitHub/Crowd-Counting-and-Monitoring/'
+MAIN_FOLDER = 'C:/Users/nizar/OneDrive/Documents/GitHub/PFE/'
