519674d7a3bed2999bd6aa3b199ee9900f24bab1
Nizar Masmoudi <nizar.masmoudi@esprit.tn>
2021-02-12 18:17:27 +0100

    Delete env.py

--- Human Detection Workspace/utils/env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DATASET_ = os.path.join('..', '..', 'VisDrone2019')
-TRAIN_PATH_ = os.path.join(DATASET_, 'train')
-TEST_PATH_ = os.path.join(DATASET_, 'test')
-VALIDATION_PATH_ = os.path.join(DATASET_, 'validation')
