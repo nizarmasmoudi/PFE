c42aed57f15f54bd6d8ee4e427dc3331d406d535
Nizar Masmoudi <nizar.masmoudi@esprit.tn>
2021-02-13 10:43:38 +0100

    Update env.py

--- Human-Detection/utils/env.py
+++ Human-Detection/utils/env.py
@@ -1,6 +1,9 @@
-import os
+import configparser
 
-DATASET_ = os.path.join('..', '..', '..', 'VisDrone2019')
-TRAIN_PATH_ = os.path.join(DATASET_, 'train')
-TEST_PATH_ = os.path.join(DATASET_, 'test')
-VALIDATION_PATH_ = os.path.join(DATASET_, 'validation')
+config = configparser.ConfigParser()
+config.read('../../env.cfg')
+
+DATASET_PATH = config['DATASET']['MAIN_PATH']
+TRAIN_PATH = config['DATASET']['TRAIN_PATH']
+VALID_PATH = config['DATASET']['VALID_PATH']
+TEST_PATH = config['DATASET']['TEST_PATH']
