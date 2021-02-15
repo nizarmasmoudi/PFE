0d216fdc8ed9dde8ada46286b10117b84969c4b0
Nizar Masmoudi <nizar.masmoudi@esprit.tn>
2021-02-15 10:21:12 +0100

    Update env.py

--- utils/env.py
+++ utils/env.py
@@ -1,12 +1,4 @@
-import configparser
-import os
-config = configparser.ConfigParser()
-if os.path.exists('../env.cfg'):
-    config.read('../env.cfg')
-else:
-    raise FileNotFoundError('Configuration file not found!')
-
-DATASET_PATH = config['DATASET']['MAIN_PATH']
-TRAIN_PATH = config['DATASET']['TRAIN_PATH']
-VALID_PATH = config['DATASET']['VALID_PATH']
-TEST_PATH = config['DATASET']['TEST_PATH']
+DATASET_PATH = 'C:\\Users\\nizar\\OneDrive\\Documents\\VisDrone2019'
+TRAIN_PATH = 'C:\\Users\\nizar\\OneDrive\\Documents\\VisDrone2019\\train'
+VALID_PATH = 'C:\\Users\\nizar\\OneDrive\\Documents\\VisDrone2019\\validation'
+TEST_PATH = 'C:\\Users\\nizar\\OneDrive\\Documents\\VisDrone2019\\test'
