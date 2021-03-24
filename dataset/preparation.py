8e6ae9478f5abc4ad262a08d5757957f1d07aa9e
Nizar Masmoudi <nizar.masmoudi@esprit.tn>
2021-03-24 10:44:56 +0100

    I hate this!!

--- dataset/preparation.py
+++ dataset/preparation.py
@@ -1,3 +1,4 @@
+from numpy.lib.npyio import save
 import pandas as pd
 import numpy as np
 import cv2 as cv
@@ -68,12 +69,16 @@ class Image:
         height, width = target
         self.mat = np.pad(self.mat, ((0, max(height - self.height, 0)), (0, max(width - self.width, 0)), (0, 0)), 'constant', constant_values=0)
         
-    def split(self, save_images=None, save_annotations=None):
+    def split(self, save_images=None, save_annotations=None, target=(736, 736)):
         '''Split image into 4 equal splits. Annotations will be split as well with dimensions taken into consideration.
 
         Args:
             save_images (string, optional): Path to parent directory where split images will be saved. Defaults to None. If not specified split images will be returned.
             save_annotations (string, optional): Path to parent directory where split annotations will be saved. Defaults to None. If not specified split annotations will be returned.
+            target (tuple, optional): Target shape of split images. Defaults to (736, 736).
+
+        Returns:
+            tuple: Annotations and matrices of split images
         '''
         images = [
             self.mat[:self.height//2, :self.width//2, :], # Top left 
@@ -81,6 +86,8 @@ class Image:
             self.mat[self.height//2:, :self.width//2, :], # Bottom left
             self.mat[self.height//2:, self.width//2:, :], # Bottom right
         ]
+        #! This is the only changement
+        images = [np.pad(image, ((0, max(target[0] - self.height//2, 0)), (0, max(target[1] - self.width//2, 0)), (0, 0)), 'constant', constant_values=0) for image in images]
         
         map_ = {'top_left': [], 'top_right': [], 'bottom_left': [], 'bottom_right': []}
         objects = self.annotations.loc[self.annotations['object_category'] != 0]
@@ -109,24 +116,38 @@ class Image:
             for i, loc in enumerate(map_.keys()):
                 map_[loc].to_csv(os.path.join(save_annotations, self.title.replace('.jpg', '_{}.txt'.format(i + 1))), header=False, index=False)
         else:
-            return map_
-        
+            return map_, images
+      
     def parse_annotations(self, save_output=None):
-        if len(self.annotations) > 0:
-            parsed = []
-            for left, top, width, height, obj_cat in self.annotations.values:
-                x_center = (left + width/2) / self.width
-                y_center = (top + height/2) / self.height
-                width = width/self.width
-                height = height/self.height
-                parsed.append('0 {} {} {} {}\n'.format(x_center, y_center, width, height))
-            if save_output:
-                with open(os.path.join(save_output, self.title.replace('.jpg', '.txt')), 'w+') as out:
-                    out.writelines(parsed)
-            else:
-                return parsed
+        annotations = self.annotations.copy().loc[self.annotations['object_category'] > 0, :]
+        annotations['x_center'] = (annotations['left'] + annotations['width']/2)/self.width
+        annotations['y_center'] = (annotations['top'] + annotations['height']/2)/self.height
+        annotations['width'] = annotations['width']/self.width
+        annotations['height'] = annotations['height']/self.height
+        annotations['object_category'] = 0
+        annotations = annotations.loc[:, ['object_category', 'x_center', 'y_center', 'width', 'height']]
+        if save_output:
+            annotations.to_csv(os.path.join(save_output, os.path.basename(self.ann_path)), sep=' ')
         else:
-            if save_output:
-                open(os.path.join(save_output, self.title.replace('.jpg', '.txt')), 'w+').close()
-            else:
-                return []
+            return annotations
+          
+      
+    # def parse_annotations(self, save_output=None):
+    #     if len(self.annotations) > 0:
+    #         parsed = []
+    #         for left, top, width, height, obj_cat in self.annotations.values:
+    #             x_center = (left + width/2) / self.width
+    #             y_center = (top + height/2) / self.height
+    #             width = width/self.width
+    #             height = height/self.height
+    #             parsed.append('0 {} {} {} {}\n'.format(x_center, y_center, width, height))
+    #         if save_output:
+    #             with open(os.path.join(save_output, self.title.replace('.jpg', '.txt')), 'w+') as out:
+    #                 out.writelines(parsed)
+    #         else:
+    #             return parsed
+    #     else:
+    #         if save_output:
+    #             open(os.path.join(save_output, self.title.replace('.jpg', '.txt')), 'w+').close()
+    #         else:
+    #             return []
